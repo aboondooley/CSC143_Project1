22b63d1e300d1eca1e0f6803671ccdb91cb5ab10
Alie Boon-Dooley <asbd@uw.edu>
2019-10-15 20:53:49 -0700

    Added some comments before turning in

--- src/world/Supermarket.java
+++ src/world/Supermarket.java
@@ -9,38 +9,41 @@ public class Supermarket extends Store {
     public Supermarket(String location, String company) {
         super(location, company);
     }
-    // Need to create our list of supplies as well
-    // protected HashMap<String, Integer> supplies = new HashMap<>();
+
 
     @Override
     public Collection<Pair<String, Integer>> getProducts() {
-        // Creates an array list the size of the HashMap that contains the products
-        //int number = this.supplies.size();
+        // Creates a blank list for us to return to the client
         ArrayList<Pair<String, Integer>> productList = new ArrayList<>();
-        /* YOUR CODE HERE */
+        // Create a set with all the keys in supplies so we can iterate over
+        // keySet() is iterable
         Set<String> keys = this.supplies.keySet();
         for (String key : this.supplies.keySet()) {
-            // build new pair, append that pair to the end of the list, then at the end, return this list
+            // Build new pair, append that pair that matches the pair from the supplies
             productList.add(new Pair<>(key, this.supplies.get(key)));
         }
+        // Return the list of products in store
         return productList;
     }
 
     /* YOUR CODE HERE */
     @Override
     public Collection<Pair<String, Integer>> purchase(Collection<Pair<String, Integer>> order) {
-        Collection<Pair<String, Integer>> returnOrder = new ArrayList<Pair<String, Integer>>();
-        // you can append a new node (pair) for each item purchased
+        // Create a new list to return, don't modify the list that is the parameter
+        Collection<Pair<String, Integer>> returnOrder = new ArrayList<>();
+        // You can append a new node (pair) for each item purchased
+        // For each item in the order, create a pair that matches
             for (Pair<String, Integer> o : order){
                 if (this.supplies.containsKey(o.left)){
                     if (o.right>=this.supplies.get(o.left)){
-                        // if the value of the purchase is greater than what is in stock, return all of what is in stock
+                        // If the number requested in the order is greater than what is in stock, add all of what is in stock
                         returnOrder.add(new Pair<>(o.left, this.supplies.get(o.left)));
-                        // so this creates a new pair, it doesn't reuse the old one, then append this pair to end of new collection list that I created.
                         // Then set what's left in the supplies set to 0
                         this.supplies.put(o.left, 0);
-                    } else { // inventory is larger than order
+                    } else { // If the number in inventory is larger than the number requested
+                        //  Add a pair with the exact number requested
                         returnOrder.add(new Pair<>(o.left, o.right));
+                        // Subtract the number ordered from the inventory
                         this.supplies.put(o.left, this.supplies.get(o.left)-o.right);
 
                     }
