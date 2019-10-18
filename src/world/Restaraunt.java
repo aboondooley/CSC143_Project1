f3e163a629501041bd4b9b5295a028e8d13163d6
Alie <asbd@uw.edu>
2019-10-17 22:27:44 -0700

    Final commit, I am almost there, just need to figure out getValue() or get().

--- src/world/Restaraunt.java
+++ src/world/Restaraunt.java
@@ -3,12 +3,14 @@ package world;
 import lib.Pair;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 
+import static java.util.Collections.max;
 import static java.util.Collections.min;
-//import static java.util.Collections;
+import static java.util.Collections.singleton;
+
 
 public class Restaraunt extends Store {
     public HashMap<String, Collection<Pair<String, Integer>>> recipes = new HashMap<>();
@@ -16,25 +18,18 @@ public class Restaraunt extends Store {
         super(location, company);
     }
 
-    public static void main(String args[]){
-        Restaraunt pizzaPlace = new Restaraunt("Inner Campus Drive", "Safeway");
-        pizzaPlace.learnRecipe("Pizza", Arrays.asList(new Pair<>("crust", 1), new Pair<>("sauce", 1)));
-        pizzaPlace.supply(Arrays.asList(new Pair<>("sauce", 5), new Pair<>("crust", 5)));
-
-        Collection<Pair<String, Integer>> result = pizzaPlace.getProducts();
-    }
     @Override
     public Collection<Pair<String, Integer>> purchase(Collection<Pair<String, Integer>> order) {
        ArrayList<Pair<String, Integer>> returnOrder = new ArrayList<>();
-        // for each recipe in the order. burger(String), 5 (int)
        for (Pair<String, Integer> p : order) {
            Collection<Pair<String, Integer>> currentStock = this.getProducts();
-           // Fill is the number of orders we need to fill
+           //int fill = 0;
+           // Fill is the number of orders we can fill
            int fill = min(currentStock.get(p.left), p.right);
-           // For each ingredient, multiply fill by the number needed in the recipe,
-           // then subtract this number from the number in supply
            for (Pair<String, Integer> i : recipes.get(p.right)) {
+               // Return the number of we can fill in our return list
                    returnOrder.add(new Pair<>(p.left, fill));
+               // Then multiply fill by the number needed in the recipe and subtract from the number in supply
                        this.supplies.put(i.left, i.right-(i.right*fill));
            }
        }
@@ -46,27 +41,23 @@ public class Restaraunt extends Store {
     public Collection<Pair<String, Integer>> getProducts() {
         // Keeps a list of how many servings of each recipe can be made
         ArrayList<Pair<String, Integer>> recipeInventory = new ArrayList<>();
-        // Loop through each recipe
        for(String r : this.recipes.keySet()){
            // In each recipe, loop through each ingredient
-           // Save a counter for the lowest denominator
-           // Collections with an S!!
-
+           // Save a counter for the lowest denominator, start it at the highest value of ingredients
            int totalAmount = max(this.supplies.values());
            for (Pair<String, Integer> i : recipes.get(r)){
-               int currentAmount = this.supplies.get(i.left);
+               //int currentAmount = this.supplies.get(i.left);
                // Need to see how many servings can be made of each ingredient in the supply room
-               //Pair<String, Integer> currentRecipe = new Pair<String, Integer>(r, this.recipes.get(i));
                int quantity = this.supplies.get(i.left) / i.right;
-               //Integer inventory = Integer.valueOf(this.supplies.get(i.left));
                if (quantity < totalAmount){
+                   // If the counter is bigger than the current number, update counter
                    totalAmount = quantity;
                }
            }
+           // Add the recipe and the number that can be made to the return list
            recipeInventory.add(new Pair<String, Integer>(r, totalAmount));
        }
         return recipeInventory;
-
     }
 
     public void learnRecipe(String name, Collection<Pair<String, Integer>> ingredients) {
